965449ffcdd5d4769e720be46b1f22c00d5446e6
bautistabernabe918-wq <bautistabernabe918@gmail.com>
2026-09-03 14:28:12 -0300

    reemplazo

--- login.php
+++ login.php
@@ -1,1 +1,32 @@
-hola como estas 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<h1>Formulario Login</h1>
+
+<form action="procesoLogin.php" method="get">
+
+
+
+<div class="mb-3">
+  <label for="formGroupExampleInput" class="form-label">Usuario</label>
+  <input type="text" class="form-control" id="Nombre" placeholder="Example input placeholder">
+</div>
+<div class="mb-3">
+  <label for="formGroupExampleInput2" class="form-label">Contraseña</label>
+  <input type="password" class="form-control" id="Contraseña" placeholder="Another input placeholder">
+</div>
+
+<button type="submit">Enviar datos</button>
+
+</form>
+
+</body>
+</html>
+
+
