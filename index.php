b8f7bd4ae0a205f1ab687ec23a39aaac6868406a
Gabriel Dobler <gabrielignaciodobler@gmail.com>
2026-09-22 15:22:44 -0300

    index llamada a procesologin en vez de procesoLogin, en el server probablemente corriendo en Linux (case sensitive) no se encontraba la direccion al archivo, arreglado

--- index.php
+++ index.php
@@ -36,7 +36,7 @@
       </svg>
     </div>
 
-    <form action="procesologin.php" method="post">
+    <form action="procesoLogin.php" method="post">
         <input type="text" id="usuario" name="usuario" placeholder="Usuario" required>
         <input type="password" id="password" name="password" placeholder="Contraseña" required>
         <button type="submit" id="btnSubmit" disabled>Entrar</button>
